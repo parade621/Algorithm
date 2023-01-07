03c0b2e2c6846abee4927ff31574f3b67baa6642
몰름보반장 <36446270+parade621@users.noreply.github.com>
2023-01-07 21:01:42 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,13 +1,15 @@
-x_nums = []
-y_nums = []
-for _ in range(3):
-    x, y = map(int, input().split())
-    x_nums.append(x)
-    y_nums.append(y)
+x=[]
+y=[]
+xr,yr= 0 ,0
+for i in range(3):
+    a,b = map(int, input().split())
+    x.append(a)
+    y.append(b)
 
 for i in range(3):
-    if x_nums.count(x_nums[i]) == 1:
-        x4 = x_nums[i]
-    if y_nums.count(y_nums[i]) == 1:
-        y4 = y_nums[i]
-print(x4, y4)
+    if x.count(x[i]) == 1:
+        xr= x[i]
+    if y.count(y[i]) == 1:
+        yr=y[i]
+
+print(xr,yr)
