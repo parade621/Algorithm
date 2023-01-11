1e8922c3330942d36fa57fa536a0db505c816413
몰름보반장 <36446270+parade621@users.noreply.github.com>
2023-01-11 12:09:45 +0900

    [Bronze I] Title: 단어 공부, Time: 180 ms, Memory: 39248 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,15 @@
+s = input().upper()
+d = dict.fromkeys(tuple(s),0)
+for i in s:
+    d[i] +=1
+tmp = max(d.values())
+c = 0
+r=""
+for i in d:
+    if d.get(i) == tmp:
+        r=i
+        c+=1
+if c == 1:
+    print(r)
+else:
+    print('?')
