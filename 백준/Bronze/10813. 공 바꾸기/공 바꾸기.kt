202575fc51b5eaa117d3e1953766b13dad142f61
몰름보반장 <36446270+parade621@users.noreply.github.com>
2023-05-29 01:02:47 +0900

    [Bronze II] Title: 공 바꾸기, Time: 156 ms, Memory: 23648 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.kt
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.kt
@@ -1,17 +1,26 @@
-fun main(){
-    val input = readLine()!!.split(" ").map{it.toInt()}
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun swap(array: Array<Int>, index1: Int, index2: Int) {
+    val temp = array[index1]
+    array[index1] = array[index2]
+    array[index2] = temp
+}
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+
+    val input = reader.readLine().split(" ").map { it.toInt() }
     val N = input[0]
     val M = input[1]
 
-    val array = Array(N) {it + 1}.toIntArray()
+    val array = Array(N) { it + 1 }
 
-    repeat(M){
-        val input = readLine()!!.split(" ").map{it.toInt()}
-        val pointer1 = input[0]-1
-        val pointer2 = input[1]-1
-        val tmp = array[pointer1]
-        array[pointer1] = array[pointer2]
-        array[pointer2] = tmp
+    repeat(M) {
+        val input = reader.readLine().split(" ").map { it.toInt() }
+        val pointer1 = input[0] - 1
+        val pointer2 = input[1] - 1
+        swap(array, pointer1, pointer2)
     }
 
     println(array.joinToString(" "))
