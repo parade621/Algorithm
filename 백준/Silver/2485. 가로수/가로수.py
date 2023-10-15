10243676f161ee14ec4e84b328c68e3740d52c9e
몰름보반장 <36446270+parade621@users.noreply.github.com>
2023-10-15 23:17:58 +0900

    [Silver IV] Title: 가로수, Time: 136 ms, Memory: 38976 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+l=deque()
+d=deque()
+r=deque()
+l = [int(input()) for _ in range(n)]
+d = [l[i+1] - l[i] for i in range(n-1)]
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+g = d[0]
+for i in range(1, len(d)):
+    g = gcd(g, d[i])
+
+t=0
+for i in d:
+    t += (i//g)-1
+
+print(t)
