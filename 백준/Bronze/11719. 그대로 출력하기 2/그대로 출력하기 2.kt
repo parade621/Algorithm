0c9c97396bcceb6f3cb25f5f1e0969de6fcb31a7
Morlmbo <36446270+parade621@users.noreply.github.com>
2024-02-05 23:56:33 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 88 ms, Memory: 12072 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.kt
@@ -1,4 +1,17 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
 fun main() {
-    val inputLines = generateSequence { readLine() }.take(100).toList()
-    inputLines.forEach { println(it) }
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    while(true){
+        val line = br.readLine()
+        if (line == null) break
+        bw.write(line)
+        bw.newLine()
+    }
+    bw.flush()
+    bw.close()
 }
